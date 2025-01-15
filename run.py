79c5a895c2d66d03ac1f531c6ba57d69af072db4
Daniel Elde-Johansson <biz45dy2@students.codeinstitute.net>
2025-01-15 09:33:44 +0000

    Added imports and scope and more gspread

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
 import random
 import re
 import time
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 # Define ships globally
 ships = {
@@ -216,8 +229,3 @@ if __name__ == '__main__':
 
 
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
