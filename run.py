847a1ab682c6d594bb6f36a12b4ce1af0fe38d5f
Daniel Elde-Johansson <biz45dy2@students.codeinstitute.net>
2025-01-17 18:54:59 +0000

    back to normal

--- run.py
+++ run.py
@@ -100,9 +100,10 @@ def computer_guess(board, last_hit=None):
     if last_hit:
         row, col = last_hit
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        for dr, dc in directions:new_row, new_col = row + dr, col + dc 
-        if 0<=new_row < 9 and 0 <=new_col < 9 and board[new_row][
-        new_col] == ' ':return new_row, new_col
+        for dr, dc in directions:
+            new_row, new_col = row + dr, col + dc
+            if 0 <= new_row < 9 and 0 <= new_col < 9 and board[new_row][new_col] == ' ':
+                return new_row, new_col
 
     while True:
         row = random.randint(0, 8)
