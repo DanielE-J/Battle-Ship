afef4791cad28440b6450e321c0e75fffe791b9e
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-03-04 13:45:09 +0100

    Tried added so computer hits the target

--- run.py
+++ run.py
@@ -99,13 +99,16 @@ def computer_guess(board, last_hit=None):
     """
     if last_hit:
         row, col = last_hit
+        # Directions: right, down, left, up
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        random.shuffle(directions)  # Randomize directions to add unpredictability
+        
         for dr, dc in directions:
             new_row, new_col = row + dr, col + dc
-            if 0 <= new_row < 9 and 0 <= new_col < 9 and board[
-             new_row][new_col] == ' ':
+            if 0 <= new_row < 9 and 0 <= new_col < 9 and board[new_row][new_col] == ' ':
                 return new_row, new_col
 
+    # If no last hit or no valid adjacent spots, choose a random empty cell
     while True:
         row = random.randint(0, 8)
         col = random.randint(0, 8)
