ce8873e64a012a5e53751f88d3b8731fd49ef1db
DanielE-J <biz45dy2@students.codeinstitute.net>
2025-03-07 13:10:26 +0100

    fixed pep8

--- run.py
+++ run.py
@@ -105,7 +105,8 @@ def computer_guess(board, last_hit=None):
 
         for dr, dc in directions:
             new_row, new_col = row + dr, col + dc
-            if 0 <= new_row < 9 and 0 <= new_col < 9 and board[new_row][new_col] == ' ':
+            if 0 <= new_row < 9 and 0 <= new_col < 9 and board[
+                 new_row][new_col] == ' ':
                 return new_row, new_col
 
     # If no last hit or no valid adjacent spots, choose a random empty cell
